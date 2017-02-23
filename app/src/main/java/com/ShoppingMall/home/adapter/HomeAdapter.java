e9be63bbe2b3d9af56a54ce9e052c86a3c1d20d0
情v枫 <1360869846@qq.com>
2017-02-23 15:37:11 +0800

    设置横幅广播的适配器

--- app/src/main/java/com/ShoppingMall/home/adapter/HomeAdapter.java
+++ app/src/main/java/com/ShoppingMall/home/adapter/HomeAdapter.java
@@ -5,13 +5,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ShoppingMall.R;
 import com.ShoppingMall.home.bean.HomeBean;
+import com.ShoppingMall.utils.Constants;
+import com.bumptech.glide.Glide;
+import com.youth.banner.Banner;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+import com.youth.banner.transformer.BackgroundToForegroundTransformer;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -99,13 +108,14 @@ public class HomeAdapter extends RecyclerView.Adapter {
 
     /**
      * 当前的类型
+     *
      * @param parent
      * @param viewType
      * @return
      */
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if(viewType == BANNER) {
+        if (viewType == BANNER) {
             return new BannerViewHolder(mContext, inflater.inflate(R.layout.banner_viewpager, null));
         } else if (viewType == CHANNEL) {
         } else if (viewType == ACT) {
@@ -118,7 +128,7 @@ public class HomeAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if(getItemViewType(position) == BANNER) {
+        if (getItemViewType(position) == BANNER) {
             BannerViewHolder viewHolder = (BannerViewHolder) holder;
             //绑定数据
             viewHolder.setData(result.getBanner_info());
@@ -130,19 +140,46 @@ public class HomeAdapter extends RecyclerView.Adapter {
         }
     }
 
-    class BannerViewHolder extends RecyclerView.ViewHolder{
+    class BannerViewHolder extends RecyclerView.ViewHolder {
 
         private final Context mContext;
-        private TextView tv_tilte;
+        private Banner banner;
 
         public BannerViewHolder(Context mContext, View itemView) {
             super(itemView);
             this.mContext = mContext;
-            tv_tilte = (TextView) itemView.findViewById(R.id.tv_tilte);
+            banner = (Banner) itemView.findViewById(R.id.banner);
         }
 
         public void setData(List<HomeBean.ResultEntity.BannerInfoEntity> banner_info) {
-            tv_tilte.setText("sssssssssssssssssssss");
+            //1、得到数据
+            //2、设置Banner数据
+            List<String> images = new ArrayList<>();
+            for (int i=0;i<banner_info.size();i++ ){
+                images.add(Constants.BASE_URL_IMAGE+banner_info.get(i).getImage());
+
+            }
+            //简单使用
+            banner.setImages(images)
+                    .setImageLoader(new ImageLoader() {
+                        @Override
+                        public void displayImage(Context context, Object path, ImageView imageView) {
+                            //具体方法内容自己去选择，次方法是为了减少banner过多的依赖第三方包，所以将这个权限开放给使用者去选择
+                            Glide.with(context).load(path).crossFade().into(imageView);
+                        }
+                    })
+                    .start();
+
+            //设置样式
+            banner.setBannerAnimation(BackgroundToForegroundTransformer.class);
+            //3、设置banner的点击事件
+            banner.setOnBannerListener(new OnBannerListener() {
+                @Override
+                public void OnBannerClick(int position) {
+                    int realPosition = position -1;
+                    Toast.makeText(mContext, "realPosition=="+realPosition, Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
