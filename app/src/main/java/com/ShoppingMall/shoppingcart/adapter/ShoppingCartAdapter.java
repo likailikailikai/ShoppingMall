4126c247b8e54e8220aded7deb8d3de01855a13f
情v枫 <1360869846@qq.com>
2017-02-28 18:34:10 +0800

    根据商品数量计算总价格

--- app/src/main/java/com/ShoppingMall/shoppingcart/adapter/ShoppingCartAdapter.java
+++ app/src/main/java/com/ShoppingMall/shoppingcart/adapter/ShoppingCartAdapter.java
@@ -15,6 +15,7 @@ import com.ShoppingMall.shoppingcart.view.AddSubView;
 import com.ShoppingMall.utils.Constants;
 import com.bumptech.glide.Glide;
 
+import java.util.Iterator;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -76,7 +77,7 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         //1、先得到数据
-        GoodsBean goodsBean = datas.get(position);
+        final GoodsBean goodsBean = datas.get(position);
         //2、绑定数据
         holder.cbGov.setChecked(goodsBean.isChecked());
         //图片
@@ -92,6 +93,18 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
         holder.addSubView.setMinValue(1);
         //设置库存--来自服务器
         holder.addSubView.setMaxValue(100);
+
+        holder.addSubView.setOnNumberChangerListener(new AddSubView.OnNumberChangerListener() {
+            @Override
+            public void onNumberChanger(int value) {
+                //1、回调数量
+                goodsBean.setNumber(value);
+
+                CartStorage.getInstance(mContext).updateData(goodsBean);
+
+                showTotalPrice();
+            }
+        });
     }
 
     @Override
@@ -129,17 +142,32 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
 
     //删除数据
     public void deleteData(){
+//        if(datas != null && datas.size()>0) {
+//            for (int i=0;i<datas.size();i++){
+//                GoodsBean goodsBean= datas.get(i);
+//                if(goodsBean.isChecked()) {
+//                    //1、内存中删除
+//                    datas.remove(goodsBean);
+//                    //2、本地也好保持
+//                    CartStorage.getInstance(mContext).deleteData(goodsBean);
+//                    //刷新数据
+//                    notifyItemRemoved(i);
+//                    i--;
+//                }
+//            }
+//        }
         if(datas != null && datas.size()>0) {
-            for (int i=0;i<datas.size();i++){
-                GoodsBean goodsBean= datas.get(i);
+            for (Iterator iterator = datas.iterator(); iterator.hasNext();){
+                GoodsBean goodsBean = (GoodsBean) iterator.next();
                 if(goodsBean.isChecked()) {
-                    //1、内存中删除
-                    datas.remove(goodsBean);
-                    //2、本地也好保持
+                    int position = datas.indexOf(goodsBean);
+                    //从内存中移除
+                    iterator.remove();
+                    //本地也要同步
                     CartStorage.getInstance(mContext).deleteData(goodsBean);
-                    //刷新数据
-                    notifyItemRemoved(i);
-                    i--;
+
+                    //刷新页面
+                    notifyItemRemoved(position);
                 }
             }
         }
