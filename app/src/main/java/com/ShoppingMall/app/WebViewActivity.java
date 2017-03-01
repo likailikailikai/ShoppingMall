ac17d751cfbcfd363de89f806a270ff180d395b0
情v枫 <1360869846@qq.com>
2017-03-01 16:28:42 +0800

    活动页面H5页面添加数据到购物车

--- app/src/main/java/com/ShoppingMall/app/WebViewActivity.java
+++ app/src/main/java/com/ShoppingMall/app/WebViewActivity.java
@@ -1,8 +1,11 @@
 package com.ShoppingMall.app;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebResourceRequest;
@@ -17,12 +20,17 @@ import android.widget.Toast;
 
 import com.ShoppingMall.R;
 import com.ShoppingMall.home.adapter.HomeAdapter;
+import com.ShoppingMall.home.bean.GoodsBean;
+import com.ShoppingMall.home.bean.H5Bean;
 import com.ShoppingMall.home.bean.WebViewBean;
 import com.ShoppingMall.utils.Constants;
+import com.alibaba.fastjson.JSON;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+
+import static com.ShoppingMall.home.adapter.HomeAdapter.GOODS_BEAN;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -91,11 +99,22 @@ public class WebViewActivity extends AppCompatActivity {
         @JavascriptInterface
         public void jumpForAndroid(String data) {
 //            Toast.makeText(WebViewActivity.this, "data==" + data, Toast.LENGTH_SHORT).show();
+            Log.e("TAG", data);
+            if (!TextUtils.isEmpty(data)) {
+                H5Bean h5Bean = JSON.parseObject(data, H5Bean.class);
 
+                //创建商品信息bean类
+                GoodsBean goodsBean = new GoodsBean();
+                goodsBean.setProduct_id(h5Bean.getValue().getProduct_id() + "");
+
+                goodsBean.setCover_price("520");
+                goodsBean.setFigure("/1478770583834.png");
+                goodsBean.setName("尚硅谷Android");
+                Intent intent = new Intent(WebViewActivity.this, GoodsInfoActivity.class);
+                intent.putExtra(GOODS_BEAN, goodsBean);
+                startActivity(intent);
+            }
         }
-
-
-
     }
 
     @OnClick({R.id.ib_back, R.id.ib_more})
